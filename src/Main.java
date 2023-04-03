b5c160357b19f4ef6fa53b739c626b682b28daee
Alex Vdovchenko <demon254@ya.ru>
2023-04-03 20:37:56 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = true;
-
-        long bonus = service.calculate(amount, registered);
-        System.out.println (bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
